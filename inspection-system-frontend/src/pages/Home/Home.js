5fca83ea63bb5199a3fbba2c0715b93ab898e198
Vu Duc Thao <thao.vd1712@gmail.com>
2023-08-08 12:09:59 -0500

    update home screen

--- inspection-system-frontend/src/pages/Home/Home.js
+++ inspection-system-frontend/src/pages/Home/Home.js
@@ -5,7 +5,24 @@ function Home() {
     return (
         <Col>
             <Header/>
-            <p> Hello</p>
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 p-5">
+                        <div className="col-12 rounded bg-info p-5">Manager</div>
+                    </div>
+                    <div className="col-6 p-5">
+                        <div className="col-12 rounded bg-info p-5">Inspector</div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6 p-5">
+                        <div className="col-12 rounded bg-info p-5">Task</div>
+                    </div>
+                    <div className="col-6 p-5">
+                        <div className="col-12 rounded bg-info p-5">Report</div>
+                    </div>
+                </div>
+            </div>
         </Col>
     );
 }
